73235ae51d8dac40ce52c62eea0c0cb67f41938b
Pierre <pierre.narcisi@epitech.eu>
2017-05-19 13:13:22 +0200

    include

---
+++ include/my.h
@@ -0,0 +1,20 @@
+/*
+** my.h for colle5 in /home/byliarus/work/colles/CPE_2016_stumper5/include/
+**
+** Made by Pierre Narcisi
+** Login   <pierre.nacisi@epitech.eu>
+**
+** Started on  Fri May 19 13:06:59 2017 Pierre Narcisi
+** Last update Fri May 19 13:12:32 2017 Pierre Narcisi
+*/
+
+#ifndef _MY_H
+# define  _MY_H
+
+typedef struct s_nb_tab
+{
+  int          nb;
+  void         (*print)(char c);
+}              t_nb_tab;
+
+#endif
